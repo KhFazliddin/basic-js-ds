82e3d6dd8daf3efba8bc3a00083185580bc74fa1
KhFazliddin <81314108+KhFazliddin@users.noreply.github.com>
2022-10-17 17:14:48 +0500

    feat: completed

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -12,7 +12,7 @@ const { ListNode } = require('../extensions/list-node.js');
  * @example
  * For l = [3, 1, 2, 3, 4, 5] and k = 3,
  * the output should be [1, 2, 4, 5]
- *
+ * Done
  * Singly - linked lists are already defined using interface
  * class ListNode {
  *   constructor(x) {
